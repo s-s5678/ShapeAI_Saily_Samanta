bd09655e99ae2b9c0feeb0a82db7a9e5f5ee16d1
s-s5678 <68969103+s-s5678@users.noreply.github.com>
2021-05-17 07:40:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+//fetching some data from the server so it's a get request.
+app.get(('/'),(req,res)=>{
+  res.sendFile(__dirname+"/index.html")
+});
+
+
+app.post(('/'),(req,res)=>{
+var n1 = Number(req.body.num1);
+var n2 = Number(req.body.num2);
+var result = n1  + n2 ;
+res.send("The answer is " + result) 
+});
+
+app.listen(3000, function(){
+    console.log("Server has started on port 3000")
+});
